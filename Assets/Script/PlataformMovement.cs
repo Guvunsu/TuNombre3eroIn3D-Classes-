dfd435e3f9b66259f4f5648e21ed0d4ad9b5c51f
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-11-04 01:51:41 -0600

    cgn b

--- Assets/Script/PlataformMovement.cs
+++ Assets/Script/PlataformMovement.cs
@@ -2,20 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlataformMovement : MonoBehaviour
-{
-    [SerializeField] Transform m_firstUpdate;
-    [SerializeField] Transform m_secondUpdate;
-    [SerializeField] float m_time;
-    // Start is called before the first frame update
-    void Start()
-    {
+public class PlataformMovement : MonoBehaviour {
+    [SerializeField] Transform m_firstPosition;
+    [SerializeField] Transform m_secondPosition;
+    [SerializeField] float m_speed = 1f;
+    private Vector3 m_targetPosition;
 
+    private void Start() {
+        // Empiezo hacia la segunda posicion a moverme
+        m_targetPosition = m_secondPosition.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector3.Slerp(m_firstUpdate.position, m_secondUpdate.position, Time.deltaTime * m_time);
+    void Update() {
+        // mi plataforma se mueve de forma recta hacia una nueva posicion a base del tiempo que le puse al del tiempo
+        transform.position = Vector3.MoveTowards(transform.position, m_targetPosition, m_speed * Time.deltaTime);
+
+        // Cuando mi distancia de donde empezo hacia la otra posicion se cumpla se cambia, haciendo un loop
+        if (Vector3.Distance(transform.position, m_targetPosition) < 0.1f) {
+            // si mi plataforma esta verdaderamente en la posicion 1 actualmente, entonces si es verdadera
+            // se cambiara a la segunda posicion, pero como no se cumple la primera condicion se regresara a la posicion 1 
+            m_targetPosition = (m_targetPosition == m_firstPosition.position) ? m_secondPosition.position : m_firstPosition.position;
+        }
     }
 }
+
+
